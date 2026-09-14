6eb9e2fbbeccbbd974344d11ca11b725e4e3f264
DiluDevX <dilumchandupa@gmail.com>
2026-09-14 11:38:59 +0530

    feat(ui): add shared RailConnect interface

---
+++ src/main/java/lk/sliit/railconnect/RailConnectApplication.java
@@ -0,0 +1,11 @@
+package lk.sliit.railconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailConnectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RailConnectApplication.class, args);
+    }
+}
